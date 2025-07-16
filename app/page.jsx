bff175ab739996646e8fbed0d0515dbc3c8b11b3
Mohammad Waqas <117596916+WaqasAsif2002@users.noreply.github.com>
2025-07-17 00:59:59 +0500

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -102,7 +102,7 @@ export default function Home() {
               </p>
               <button
                 onClick={handleBookNow}
-                className="bg-blue-600 text-white px-8 py-3 rounded-lg text-lg font-semibold hover:bg-blue-700 transition"
+                    className="bg-transparent text-white border border-white px-8 py-3 rounded-lg text-lg font-semibold hover:bg-gray-600 !hover:bg-gray-600 transition"
               >
                 Book Now
               </button>
